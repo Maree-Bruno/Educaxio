f8ddd12e43f66fbc67610b2810c8743bfed4cea6
Bruno Marée <bruno.maree@student.hepl.be>
2026-05-18 15:37:50 +0200

    make Welcome page

--- resources/js/components/widgets/LinkButton.vue
+++ resources/js/components/widgets/LinkButton.vue
@@ -3,7 +3,7 @@ import { Link } from '@inertiajs/vue3';
 import { computed } from 'vue';
 
 type Variant = 'primary' | 'secondary' | 'danger';
-type Size = 'sm' | 'md' | 'lg';
+type Size = 'sm' | 'md' | 'lg' | 'xs';
 type Method = 'get' | 'post' | 'put' | 'patch' | 'delete';
 
 const props = withDefaults(
@@ -38,9 +38,16 @@ const variantClasses = computed(
 );
 
 const sizePaddingMap = {
+    xs: { base: 'px-2 py-1 text-xs', lg: 'lg:px-4 lg:py-2 lg:text-base' },
     sm: { base: 'px-4 py-2 text-base', lg: 'lg:px-4 lg:py-2 lg:text-base' },
-    md: { base: 'px-5 py-2.5 text-xl font-bold', lg: 'lg:px-5 lg:py-2.5 lg:text-xl lg:font-bold' },
-    lg: { base: 'px-7 py-3.5 text-2xl font-bold', lg: 'lg:px-7 lg:py-3.5 lg:text-2xl lg:font-bold' },
+    md: {
+        base: 'px-5 py-2.5 text-xl font-bold',
+        lg: 'lg:px-5 lg:py-2.5 lg:text-xl lg:font-bold',
+    },
+    lg: {
+        base: 'px-7 py-3.5 text-2xl font-bold',
+        lg: 'lg:px-7 lg:py-3.5 lg:text-2xl lg:font-bold',
+    },
 };
 
 const sizeClasses = computed(() => {
@@ -52,6 +59,7 @@ const sizeClasses = computed(() => {
 });
 
 const iconSizeMap = {
+    xs: { base: 'w-2 h-2', lg: 'lg:w-2 lg:h-2' },
     sm: { base: 'w-4 h-4', lg: 'lg:w-4 lg:h-4' },
     md: { base: 'w-5 h-5', lg: 'lg:w-5 lg:h-5' },
     lg: { base: 'w-6 h-6', lg: 'lg:w-6 lg:h-6' },
