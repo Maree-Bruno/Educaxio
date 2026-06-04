259c8a745107a5a94a46445793b97be30a315171
Bruno Marée <bruno.maree@student.hepl.be>
2026-06-04 15:30:55 +0200

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -61,7 +61,16 @@ return [
             'report' => false,
         ],
 
-        'userimages' => [
+        'userimages' => env('FILESYSTEM_DISK') === 's3' ? [
+            'driver' => 's3',
+            'region' => env('AWS_DEFAULT_REGION', 'auto'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', true),
+            'visibility' => 'public',
+            'throw' => true,
+        ] : [
             'driver' => 'local',
             'root' => storage_path('app/public/images'),
             'url' => env('APP_URL').'/images',
@@ -84,7 +93,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
-        public_path('images/profile') => storage_path('app/public/images/profile'),
+        public_path('images') => storage_path('app/public/images'),
     ],
 
 ];
